254e568885768937e3006efb196894698ca09d79
Marcelo <qw3xvf@gmail.com>
2020-03-28 11:00:54 +0900

    move all app files to src

---
+++ src/dependencyInjection/container.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata'
+import { Container } from 'inversify'
+import { TYPES } from './types'
+
+// import interfaces
+//
+// import repositories
+
+const serviceContainer = new Container()
+
+export { serviceContainer }
---
+++ src/lang/ja-JP.js
@@ -0,0 +1,11 @@
+export default {
+  welcome: 'ようこそ',
+  like: 'いいね',
+  article: '記事',
+  follow: 'フォロー',
+  follower: 'フォロワー',
+  newPost: '投稿する',
+  login: 'ログイン',
+  loginWith: 'Login with ',
+  search: 'キーワードで探す',
+}
