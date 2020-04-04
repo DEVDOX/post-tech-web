c00769fe75ce89b1b35ed42e1d7fc823be9378dc
rikusen0335 <rikusen0335@gmail.com>
2020-04-04 13:39:43 +0900

    FIX logout page

--- src/pages/logout.vue
+++ src/pages/logout.vue
@@ -1,3 +1,18 @@
 <template>
-  <p>ログアウトしました。</p>
+  <div>
+    <p>ログアウトしました。</p>
+    <p>リダイレクト中...</p>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { Context } from '@nuxt/types';
+
+@Component
+export default class Header extends Vue {
+  asyncData({ redirect }: Context) {
+    redirect('/')
+  }
+}
+</script>
